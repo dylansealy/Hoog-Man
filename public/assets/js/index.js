61f98644637ed2ce7638fb7034f404b3ce233ca6
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2020-11-14 18:14:42 +0100

    Werkende p5 implementatie

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,4 +7,12 @@ const footerText = () => {
     footer.innerText = `© ${year} Nynke van der Eems, Martijn Huls, Jitse Ritskes, Dylan Sealy, Alida Terwisscha van Scheltinga, Jelte Venema`;
 }
 
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(1);
+}
+
 footerText();
