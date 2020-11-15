84b743bcc983e81d48c7a5b9c293736b4262827b
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2020-11-15 14:43:20 +0100

    Comments en social toegevoegd

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,16 +1,19 @@
+// Creëert constanten van HTML elementen.
 const footer = document.querySelector("footer");
-
+const social = document.querySelector("#social");
+// Zorgt ervoor dat de gebruiker wordt doorgestuurd naar de nieuwe website wanneer op het element wordt geklikt.
+social.addEventListener("click", () => window.location.href = "https://github.com/DylanSealy/PO-2D-games-maken");
 // Functie voor het correcte copyright jaar.
 const footerText = () => {
     let year = new Date;
     year = year.getFullYear();
     footer.innerText = `© ${year} Nynke van der Eems, Martijn Huls, Jitse Ritskes, Dylan Sealy, Alida Terwisscha van Scheltinga, Jelte Venema`;
 }
-
+// p5 setup functie
 function setup() {
     createCanvas(400, 400);
 }
-
+// p5 draw functie
 function draw() {
     background(1);
 }
