57a505d393686d75bd3449304637ff60cc868196
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2020-11-17 21:17:50 +0100

    Style veranderd

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,5 @@
 // Creëert constanten van HTML elementen.
-const footer = document.querySelector("footer");
+const copyright = document.querySelector("#copyright");
 const social = document.querySelector("#social");
 // Zorgt ervoor dat de gebruiker wordt doorgestuurd naar de nieuwe website wanneer op het element wordt geklikt.
 social.addEventListener("click", () => window.location.href = "https://github.com/DylanSealy/PO-2D-games-maken");
@@ -7,15 +7,18 @@ social.addEventListener("click", () => window.location.href = "https://github.co
 const footerText = () => {
     let year = new Date;
     year = year.getFullYear();
-    footer.innerText = `© ${year} Nynke van der Eems, Martijn Huls, Jitse Ritskes, Dylan Sealy, Alida Terwisscha van Scheltinga, Jelte Venema`;
+    copyright.innerText = `© ${year} Nynke van der Eems, Martijn Huls, Jitse Ritskes, Dylan Sealy, Alida Terwisscha van Scheltinga, Jelte Venema`;
 }
 // p5 setup functie
 function setup() {
     createCanvas(400, 400);
+    noStroke();
+    background("#b1d8d8");
 }
 // p5 draw functie
 function draw() {
-    background(1);
+    fill("green");
+    rect(100, 100, 200, 200);
 }
 
 footerText();
