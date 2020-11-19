5575077cd5badf8c82be98585678c53ee5face50
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2020-11-20 00:22:18 +0100

    Style aangepast, maps toegevoegd en ervoor gezorgd dat het canvas 'responsive' is

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,23 +1,35 @@
-// Creëert constanten van HTML elementen.
-const copyright = document.querySelector("#copyright");
+// Creëert constante van HTML element.
 const social = document.querySelector("#social");
-// Zorgt ervoor dat de gebruiker wordt doorgestuurd naar de nieuwe website wanneer op het element wordt geklikt.
-social.addEventListener("click", () => window.location.href = "https://github.com/DylanSealy/PO-2D-games-maken");
 // Functie voor het correcte copyright jaar.
 const footerText = () => {
+    const footer = document.querySelector("footer");
     let year = new Date;
     year = year.getFullYear();
-    copyright.innerText = `© ${year} HOOG-MAN`;
+    footer.innerText = `© ${year} HOOG-MAN`;
 }
-
+// Functie voor het bepalen van de dimensies van het main element
+const getMainDimensions = () => {
+    const main = document.querySelector("main");
+    const height = main.offsetHeight;
+    const width = main.offsetWidth;
+    if (width > height) {return height;}
+    else {return width;}
+}
+// Declareert benodigde variabelen voor de game.
+// vars is een object. Binnen een object hebben je key/value pairs.
+let vars = {}
+const initializeVars = () => {
+    vars.canvasDimension = getMainDimensions();
+}
 // p5 preload functie
 function preload() {
+    initializeVars();
     loadFont("assets/fonts/Roboto-Light.ttf");
 }
-
 // p5 setup functie
 function setup() {
-    createCanvas(400, 400);
+    // vars.canvasDimension - 1 om ervoor te zorgen dat er geen scrollbars komen.
+    createCanvas(vars.canvasDimension - 1, vars.canvasDimension - 1);
     noStroke();
     frameRate(60);
     textFont("Roboto");
@@ -30,5 +42,10 @@ function draw() {
     rect(100, 100, 200, 200);
     text("Text", 20, 20);
 }
+// Eventlisteners zorgen ervoor dat er iets gebeurd na een actie van de client.
+// Doorsturen naar GitHub
+social.addEventListener("click", () => window.location.href = "https://github.com/DylanSealy/PO-2D-games-maken");
+// Pagina herladen nadat de dimensie is veranderd.
+window.addEventListener("resize", () => location.reload());
 
 footerText();
