fe4ba3bed828afd6830f12d9ef33c6233ecb8503
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2020-11-20 23:34:09 +0100

    Concept map 1 voltooid

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,7 +7,7 @@ const footerText = () => {
     year = year.getFullYear();
     footer.innerText = `© ${year} HOOG-MAN`;
 }
-// Functie voor het bepalen van de dimensies van het main element
+// Functie voor het bepalen van de dimensies van het main element.
 const getMainDimensions = () => {
     const main = document.querySelector("main");
     const height = main.offsetHeight;
@@ -35,40 +35,30 @@ const initializeVars = () => {
     vars.heightUnit = vars.innerHeight / 14;
     vars.widthUnit = vars.innerWidth / 17;
 }
-// p5 preload functie
-function preload() {
-    initializeVars();
-    loadFont("assets/fonts/Roboto-Light.ttf");
-}
-// p5 setup functie
-function setup() {
-    canvas = createCanvas(vars.canvasDimension, vars.canvasDimension);
-    colorMode(RGB, 255);
-    noStroke();
-    frameRate(60);
-    textFont("Roboto");
-    textSize(20);
-    background("black");
-}
-// p5 draw functie
-function draw() {
+// functie voor het tekenen van de buitenlijnen.
+const outerLines = () => {
     // Eerste vorm altijd buitenste.
-
-    noFill();
-    stroke("#2121DE");
+    push()
     strokeWeight(3);
-    // Buitenlijnen
     rect(vars.xOuter, vars.yOuter, vars.outerWidth, vars.outerHeight, 10);
     rect(vars.xInner, vars.yInner, vars.innerWidth, vars.innerHeight, 4);
+    pop();
+}
+// Functie voor het tekenen van de buitenlijn doorgangen.
+const outerLinesPassage = () => {
+    push();
     stroke("black");
     strokeWeight(4);
-    // Buitenlijnen doorgangen
     rect(vars.xInner + vars.widthUnit, vars.yOuter, vars.widthUnit, 0);
     rect(vars.xInner + vars.widthUnit, vars.yInner, vars.widthUnit, 0);
     rect(vars.xInner + vars.widthUnit, vars.yOuter + vars.outerHeight, vars.widthUnit, 0);
     rect(vars.xInner + vars.widthUnit, vars.yInner + vars.innerHeight, vars.widthUnit, 0);
+    pop();
+}
+// Functie voor het tekenen van de gele bolletjes.
+const candy = () => {
+    push();
     stroke("yellow");
-    strokeWeight(2);
     fill("yellow");
     for (let i = 0; i < 14; i++) {
         circle(vars.xInner + vars.widthUnit * 0.5, vars.yInner + vars.heightUnit * (0.5 + i), vars.widthUnit * 0.25);
@@ -76,14 +66,103 @@ function draw() {
             circle(vars.xInner + vars.widthUnit * (0.5 + j), vars.yInner + vars.heightUnit * (0.5 + i), vars.widthUnit * 0.25);
         }
     }
-    //
-    stroke("#2121DE");
-    strokeWeight(2);
+    pop();
+}
+// Functie voor het tekenen van alle barrières.
+const obstacles = () => {
+    push();
     fill("black");
+    // Voor de volgorde zie bij maps 1.jpg.
+    // 1
     rect(vars.xInner + vars.widthUnit, vars.yInner + vars.heightUnit, vars.widthUnit * 2, vars.heightUnit * 3, 4);
+    // 2
+    rect(vars.xInner + vars.widthUnit * 4, vars.yInner, vars.widthUnit, vars.heightUnit * 4, 4);
+    // 3
+    rect(vars.xInner + vars.widthUnit * 6, vars.yInner + vars.heightUnit, vars.widthUnit * 2, vars.heightUnit * 3, 4);
+    // 4
+    rect(vars.xInner + vars.widthUnit * 9, vars.yInner, vars.widthUnit, vars.heightUnit * 3, 4);
+    // 5
+    rect(vars.xInner + vars.widthUnit * 11, vars.yInner + vars.heightUnit, vars.widthUnit * 2, vars.heightUnit * 2, 4);
+    // 6
+    rect(vars.xInner + vars.widthUnit * 14, vars.yInner, vars.widthUnit * 3, vars.heightUnit * 2, 4);
+    // 7
     rect(vars.xInner, vars.yInner + vars.heightUnit * 5, vars.widthUnit, vars.heightUnit * 3, 4);
+    // 8
+    rect(vars.xInner + vars.widthUnit * 2, vars.yInner + vars.heightUnit * 5, vars.widthUnit * 2, vars.heightUnit * 3, 4);
+    // 9
+    rect(vars.xInner + vars.widthUnit * 5, vars.yInner + vars.heightUnit * 5, vars.widthUnit * 2, vars.heightUnit, 4);
+    // 10
+    rect(vars.xInner + vars.widthUnit * 8, vars.yInner + vars.heightUnit * 5, vars.widthUnit, vars.heightUnit, 4);
+    // 11
+    rect(vars.xInner + vars.widthUnit * 9, vars.yInner + vars.heightUnit * 4, vars.widthUnit, vars.heightUnit * 3, 4);
+    // 12
+    rect(vars.xInner + vars.widthUnit * 11, vars.yInner + vars.heightUnit * 5, vars.widthUnit, vars.heightUnit, 4);
+    // 13
+    rect(vars.xInner + vars.widthUnit * 11, vars.yInner + vars.heightUnit * 4, vars.widthUnit * 5, vars.heightUnit, 4);
+    // 14
+    rect(vars.xInner + vars.widthUnit * 14, vars.yInner + vars.heightUnit * 3, vars.widthUnit * 2, vars.heightUnit, 4);
+    // 15
+    rect(vars.xInner + vars.widthUnit * 5, vars.yInner + vars.heightUnit * 7, vars.widthUnit, vars.heightUnit, 4);
+    // 16
+    rect(vars.xInner + vars.widthUnit * 7, vars.yInner + vars.heightUnit * 7, vars.widthUnit, vars.heightUnit * 3, 4);
+    // 17
+    rect(vars.xInner + vars.widthUnit * 9, vars.yInner + vars.heightUnit * 8, vars.widthUnit, vars.heightUnit * 2, 4);
+    // 18
+    rect(vars.xInner + vars.widthUnit * 10, vars.yInner + vars.heightUnit * 9, vars.widthUnit, vars.heightUnit * 2, 4);
+    // 19
+    rect(vars.xInner + vars.widthUnit * 11, vars.yInner + vars.heightUnit * 7, vars.widthUnit * 3, vars.heightUnit, 4);
+    // 20
+    rect(vars.xInner + vars.widthUnit * 13, vars.yInner + vars.heightUnit * 6, vars.widthUnit, vars.heightUnit, 4);
+    // 21
+    rect(vars.xInner + vars.widthUnit * 15, vars.yInner + vars.heightUnit * 6, vars.widthUnit, vars.heightUnit * 2, 4);
+    // 22
     rect(vars.xInner + vars.widthUnit, vars.yInner + vars.heightUnit * 9, vars.widthUnit * 2, vars.heightUnit * 4, 4);
-
+    // 23
+    rect(vars.xInner + vars.widthUnit * 4, vars.yInner + vars.heightUnit * 9, vars.widthUnit * 2, vars.heightUnit * 3, 4);
+    // 24
+    rect(vars.xInner + vars.widthUnit * 7, vars.yInner + vars.heightUnit * 11, vars.widthUnit * 2, vars.heightUnit, 4);
+    // 25
+    rect(vars.xInner + vars.widthUnit * 8, vars.yInner + vars.heightUnit * 12, vars.widthUnit, vars.heightUnit, 4);
+    // 26
+    rect(vars.xInner + vars.widthUnit * 12, vars.yInner + vars.heightUnit * 9, vars.widthUnit, vars.heightUnit * 3, 4);
+    // 27
+    rect(vars.xInner + vars.widthUnit * 14, vars.yInner + vars.heightUnit * 9, vars.widthUnit * 3, vars.heightUnit, 4);
+    // 28
+    rect(vars.xInner + vars.widthUnit * 4, vars.yInner + vars.heightUnit * 13, vars.widthUnit * 3, vars.heightUnit, 4);
+    // 29
+    rect(vars.xInner + vars.widthUnit * 10, vars.yInner + vars.heightUnit * 12, vars.widthUnit, vars.heightUnit * 2, 4);
+    // 30
+    rect(vars.xInner + vars.widthUnit * 11, vars.yInner + vars.heightUnit * 13, vars.widthUnit * 3, vars.heightUnit, 4);
+    // 31
+    rect(vars.xInner + vars.widthUnit * 14, vars.yInner + vars.heightUnit * 11, vars.widthUnit * 2, vars.heightUnit, 4);
+    // 32
+    rect(vars.xInner + vars.widthUnit * 15, vars.yInner + vars.heightUnit * 12, vars.widthUnit, vars.heightUnit, 4);
+    pop();
+}
+// p5 preload functie
+function preload() {
+    initializeVars();
+    loadFont("assets/fonts/Roboto-Light.ttf");
+    loadSound();
+}
+// p5 setup functie
+function setup() {
+    canvas = createCanvas(vars.canvasDimension, vars.canvasDimension);
+    colorMode(RGB, 255);
+    frameRate(60);
+    textFont("Roboto");
+    textSize(20);
+    background("black");
+}
+// p5 draw functie
+function draw() {
+    noFill();
+    strokeWeight(2)
+    stroke("#2121DE");
+    outerLines();
+    outerLinesPassage();
+    candy();
+    obstacles();
 }
 // Eventlisteners zorgen ervoor dat er iets gebeurd na een actie van de client.
 // Doorsturen naar GitHub
