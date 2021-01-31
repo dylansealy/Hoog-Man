5644c89dd2fe0b5cf17f65499c91c742fee7c3a5
Dylan Sealy <66305635+DylanSealy@users.noreply.github.com>
2021-01-31 22:06:52 +0100

    Versie geupdatet naar 1.1.0

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const staticCache = "staticCacheV1.0.3";
-const dynamicCache = "dynamicCacheV1.0.3";
+const staticCache = "staticCacheV1.1.0";
+const dynamicCache = "dynamicCacheV1.1.0";
 const cacheAssets = [
     "/Hoog-Man/dist/index.html",
     "/Hoog-Man/dist/assets/html/fallback.html",
